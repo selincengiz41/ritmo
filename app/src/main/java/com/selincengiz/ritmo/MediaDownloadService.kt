8680804d36fdd503ab3c1527ae41578f271c2fce
Selin Cengiz <selincengiz41@gmail.com>
2024-08-08 10:03:39 +0300

    notification solved

--- app/src/main/java/com/selincengiz/ritmo/MediaDownloadService.kt
+++ app/src/main/java/com/selincengiz/ritmo/MediaDownloadService.kt
@@ -7,8 +7,8 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
-import android.util.Log
 import androidx.annotation.OptIn
+import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.common.util.Util
@@ -32,59 +32,47 @@ class MediaDownloadService : DownloadService(
 
     private lateinit var downloadManager: DownloadManager
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate() {
         super.onCreate()
-//TODO:Notification
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val notificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            val channel = NotificationChannel(
-                DOWNLOAD_NOTIFICATION_CHANNEL_ID,
-                getString(R.string.download_notification_channel_name),
-                NotificationManager.IMPORTANCE_LOW
-            ).apply {
-                description = getString(R.string.download_notification_channel_description)
-            }
-            notificationManager.createNotificationChannel(channel)
-        }
-
         val notification = getForegroundNotification(emptyList(), 0)
         startForeground(FOREGROUND_NOTIFICATION_ID, notification)
     }
 
     override fun getDownloadManager(): DownloadManager {
-        downloadManager = DownloadUtil.getDownloadManager(this)
-
+        downloadManager = DownloadUtil.getDownloadManager(applicationContext)
         return downloadManager
     }
 
     override fun getScheduler(): Scheduler {
-        return PlatformScheduler(this, JOB_ID)
+        return PlatformScheduler(applicationContext, JOB_ID)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun getForegroundNotification(
         downloads: List<Download>,
         notMetRequirements: Int
     ): Notification {
         val notificationManager =
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(
-                DOWNLOAD_NOTIFICATION_CHANNEL_ID,
-                getString(R.string.download_notification_channel_name),
-                NotificationManager.IMPORTANCE_LOW
-            ).apply {
-                description = getString(R.string.download_notification_channel_description)
-            }
-            notificationManager.createNotificationChannel(channel)
+        val channel = NotificationChannel(
+            DOWNLOAD_NOTIFICATION_CHANNEL_ID,
+            getString(R.string.download_notification_channel_name),
+            NotificationManager.IMPORTANCE_DEFAULT
+        ).apply {
+            description = getString(R.string.download_notification_channel_description)
         }
-        val builder = NotificationCompat.Builder(this, DOWNLOAD_NOTIFICATION_CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_download)
-            .setContentTitle(getString(R.string.download_notification_title))
-            .setContentText(getString(R.string.download_notification_text))
-            .setPriority(NotificationCompat.PRIORITY_LOW)
-            .setOngoing(true)
+        notificationManager.createNotificationChannel(channel)
 
+        val builder =
+            NotificationCompat.Builder(applicationContext, DOWNLOAD_NOTIFICATION_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_download)
+                .setContentTitle(getString(R.string.download_notification_title))
+                .setContentText(getString(R.string.download_notification_text))
+                .setPriority(NotificationCompat.PRIORITY_LOW)
+                .setOngoing(true)
+
+        notificationManager.notify(1, builder.build())
         return builder.build()
     }
 
@@ -115,12 +103,11 @@ class MediaDownloadService : DownloadService(
                     download: Download,
                     finalException: Exception?
                 ) {
-                    if(download.state == Download.STATE_COMPLETED){
+                    if (download.state == Download.STATE_COMPLETED) {
                         finished()
                     }
                 }
             })
-
         }
     }
 }
